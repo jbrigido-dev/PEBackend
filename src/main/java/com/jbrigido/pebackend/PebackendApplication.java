a4ede9eaf325d320054b7b930de7f039ffcd11cf
Brigido <jbrigidoperez@outlook.com>
2026-05-21 15:40:43 -0600

    Configured Database

---
+++ src/main/java/com/jbrigido/pebackend/PebackendApplication.java
@@ -0,0 +1,13 @@
+package com.jbrigido.pebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PebackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbrigido/pebackend/PebackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbrigido.pebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PebackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
