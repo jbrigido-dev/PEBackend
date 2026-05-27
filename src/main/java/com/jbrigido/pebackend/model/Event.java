4a20b722959f10acfcb5ab3393af722152b72a6c
user <jbrigidoperez@outlook.com>
2026-05-27 14:39:42 -0600

    Event entity added

---
+++ src/main/java/com/jbrigido/pebackend/model/Event.java
@@ -0,0 +1,58 @@
+package com.jbrigido.pebackend.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Event {
+    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ev_id")
+    private Long id;
+    @Column(name = "ev_name", nullable = false , length = 100)
+    private String name;
+    @Column(name = "ev_description", nullable = true , length = 200)
+    private String description;
+    @Column(name = "ev_start_date", nullable = false)
+    private LocalDate starDate;
+    @Column(name = "ev_end_date", nullable = true)
+    private LocalDateTime endDate;
+    @Column(name = "ev_status", nullable = false)
+    private Boolean status;
+    @Column(name = "ev_created_at", nullable = false)
+    private LocalDate createdAt;
+
+    @PrePersist
+    public void prePersist(){
+        if (starDate == null) {
+            starDate = LocalDate.now();
+        }
+        if (endDate == null) {
+            endDate = LocalDateTime.now();
+        }
+        if (createdAt == null) {
+                createdAt= LocalDate.now();
+        }
+        if (status == null) {
+            status = true;
+        }
+    }
+    
+}
